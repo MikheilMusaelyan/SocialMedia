6b6b3f7a28ae1c45710442991131fb8dfe92b270
unknown <mishkamusaeliani@gmail.com>
2023-01-06 16:25:32 -0500

    igive

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const activeUsersRoutes = require('./auth/activeUsers');
 const messages = require('./messages/messages');
 const cors = require('cors');
 
-
-app.use(cors({
-  origin: 'http://localhost:4200'
-}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/images', express.static('backend/images'))
@@ -29,6 +25,19 @@ mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@mikescluster.aootk
 .catch(err => {
     console.log('unable to connect!')
 })
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, auth'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'POST, GET, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+});
 
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
