9b688847cbdba1ad4101107677b24977c9cd6c1b
unknown <mishkamusaeliani@gmail.com>
2023-01-17 11:07:47 -0500

    dmessage

--- posts/posts.js
+++ posts/posts.js
@@ -142,13 +142,11 @@ router.get('/singlePost', (req, res) => {
 router.get('/allPosts/:incAmount', (req, res, next) => {
     const increasingAmount = +req.params.incAmount;
     Post.count().then(postCount => {
-        let toSkip = postCount - increasingAmount;       
+        let toSkip = postCount - increasingAmount;   
         
         if(toSkip <= 0){
             toSkip = 0;
         };
-
-        console.log(toSkip)
 
         Post.aggregate([
             { $skip: 0 },
