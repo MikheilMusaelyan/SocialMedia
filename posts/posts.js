c0ae8000d8367184519bedf3ccdb456fe0765bc3
unknown <mishkamusaeliani@gmail.com>
2023-01-22 14:25:58 -0500

    dsa

--- posts/posts.js
+++ posts/posts.js
@@ -12,7 +12,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const upload = exportsFile.upload;
-const cloudinary = require('cloudinary').v2.config({
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
