e692e4d6aed36daf2c4b98be04216220c1148892
unknown <mishkamusaeliani@gmail.com>
2023-02-09 20:39:34 -0500

    message

--- auth/auth.js
+++ auth/auth.js
@@ -32,6 +32,7 @@ router.get('/searchUsers', (req, res) => {
                     _id: "$_id",
                     nickname: "$nickname",
                     profilePic: "$afterLogin.profilePic",
+                    notifications: '$notifications'
                 }
             }
         },
